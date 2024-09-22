a5fd7f6b7a2ef9e55d2177bced15f6be99cbccdc
Gustavo Emanuel <gustavoe.dev@gmail.com>
2024-09-21 21:18:21 -0300

    stylized Home and History

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,48 @@
+import { Play } from "phosphor-react";
+import {
+  ButtonContainer,
+  CounterContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesInput,
+  ProjectInput,
+  Separator,
+} from "./styles";
+
 export function Home() {
-  return <h1>Home</h1>;
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em </label>
+          <ProjectInput id="task" placeholder="Dê um nome para o seu projeto" />
+
+          <label htmlFor="minutes">durante</label>
+          <MinutesInput
+            type="number"
+            id="minutes"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CounterContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CounterContainer>
+
+        <ButtonContainer type="submit">
+          <Play size={24} />
+          Começar
+        </ButtonContainer>
+      </form>
+    </HomeContainer>
+  );
 }
